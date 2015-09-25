8ee0e59a4ff22373ff119c0133c73b6670ca4033
Michael Bishoff <bishoff1@umbc.edu>
2015-09-24 20:04:18 -0400

    Adding website

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function buttonPressed() {
+    var message = document.getElementById("message").value;
+    
+    if (message) {
+        var background = document.getElementById("background");
+        background.style.background = message;
+//        alert(message);
+    }
+    else {
+        alert("No Message!");
+    }
+    
+}
+
+
